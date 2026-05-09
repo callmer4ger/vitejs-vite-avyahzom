a687855aa208031c1211fcb6a08eacbf1d848976
callmer4ger <163800085+callmer4ger@users.noreply.github.com>
2026-05-08 21:04:09 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,11 @@
-import { createClient } from '@supabase/supabase-js'; // @ts-ignore const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || ''; // @ts-ignore const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || ''; export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import { createClient } from '@supabase/supabase-js';
+
+// Busca as variáveis de ambiente do arquivo .env
+// @ts-ignore
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
+
+// @ts-ignore
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+
+// Inicializa o cliente do Supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
